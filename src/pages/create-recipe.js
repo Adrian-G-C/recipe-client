946a133cbd1737c6ecf021927fc95ad6897a35c3
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-11 16:09:42 -0500

    create recipe form input created

--- src/pages/create-recipe.js
+++ src/pages/create-recipe.js
@@ -1,4 +1,22 @@
 export const CreateRecipe = () => {
 
-    return <div> CreateRecipe</div>;
+    return ( 
+    <div className="create-recipe">
+        <h2> CreateRecipe</h2>
+        <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name"/>
+            <label htmlFor="description">Description</label>
+            <textarea id="description" name="description"></textarea>
+            <label htmlFor="ingredients">Ingredients</label>
+
+            <label htmlFor="instructions">Instructions</label>
+            <textarea id="instructions" name="instructions"></textarea>
+            <label htmlFor="imageUrl">Image URL</label>
+            <input type="text" id="imageUrl" name="imageUrl" />
+            <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+            <input type="number" id="cookingTime" name="cookingTIme" />
+        </form>
+        </div>
+    );
 };  
