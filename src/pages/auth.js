004c79b00423374ec8ab513d4983031c70f381c3
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-10 20:59:52 -0500

    auth page complete with username and password input field

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,4 +1,42 @@
-export const Auth = () => {
+import { useState } from "react";
 
-    return <div> Auth</div>;
+export const Auth = () => {
+    return (
+        <div className="auth">
+            <Login />
+            <Register />
+        </div>
+    );
+};
+
+const Login = () => {
+    return <div></div>;
+};
+
+const Register = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    return <div className="auth-container">
+        <form>
+            <h2> Register </h2>
+            <div className="form-group">
+                <label htmlFor="username"> Username: </label>
+                <input 
+                type="text" 
+                id="username"
+                value={username} 
+                onChange={(event) => setUsername(event.target.value) }/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="password"> Password: </label>
+                <input 
+                type="text" 
+                id="password"
+                 value={password}
+                onChange={(event) => setPassword(event.target.value) }/>
+            </div>
+        </form>
+    </div>;
 };
