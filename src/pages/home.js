7c17809013d7c4973deb1c8aa8559087718a5aa9
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-11 21:54:20 -0500

    test

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,7 +11,7 @@ export const Home = () => {
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/recipes");
+        const response = await axios.get("https://project3-recipeapp-848a22926a42.herokuapp.com/recipes");
         setRecipes(response.data);
       } catch (err) {
         console.log(err);
@@ -21,7 +21,7 @@ export const Home = () => {
     const fetchSavedRecipes = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:3001/recipes/savedRecipes/ids/${userID}`
+          `https://project3-recipeapp-848a22926a42.herokuapp.com/recipes/savedRecipes/ids/${userID}`
         );
         setSavedRecipes(response.data.savedRecipes);
       } catch (err) {
